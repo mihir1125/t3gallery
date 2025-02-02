0f4613790c301341f557fa5b6a06481bc9551793
Mihir Manjrekar <mihir.laxmi@gmail.com>
2025-02-02 19:01:16 +0530

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://j5aiazu0wt.ufs.sh/f/kgZEcy0TDlHAvbIDATsRVGEpcl7ymbzrUdNM8wn6Hgv35LkP",
+  "https://j5aiazu0wt.ufs.sh/f/kgZEcy0TDlHA2A85OlVhnBkWFZ5iGK3NAjdwt0So8grepmsH",
+  "https://j5aiazu0wt.ufs.sh/f/kgZEcy0TDlHAA42yQzGPsWrGXH5lC4nZ8bF9JiUhVDuR3IBp",
+  "https://j5aiazu0wt.ufs.sh/f/kgZEcy0TDlHApVHP4okQ60MHQGaOPJd2ALTW7ulYb4KxEcZI",
+];
+
+const mockImages = mockUrls.map((url, idx) => ({
+  id: idx + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, idx) => (
+            <div key={idx} className="w-48">
+              <img src={image.url}/>
+            </div>
+          ))
+        }
+      </div>
     </main>
   );
 }
